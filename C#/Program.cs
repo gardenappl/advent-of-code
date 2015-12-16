07dfe9ea9319f8296fb766ec9871deb8bb2e54a9
goldenapple3 <goldenappleminecraft@gmail.com>
2015-12-16 12:41:40 +0200

    Fixed timer and fancified the program

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,12 +1,13 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace AdventOfCode
 {
 	public class Program
 	{
-		static readonly Day[] days = new Day[25];
+		static readonly Day[] days = new Day[26];
 		
 		public static void Main()
 		{
@@ -15,26 +16,38 @@ namespace AdventOfCode
 			days[3] = new Day3();
 			days[4] = new Day4();
 			days[5] = new Day5();
+			days[6] = new Day6();
+			
+			for(int i = 1; i <= 25; i++) //A little thing that shows which days have solutions
+			{
+				if(days[i] != null)
+					Console.ForegroundColor = ConsoleColor.Green;
+				else
+					Console.ForegroundColor = ConsoleColor.Red;
+				Console.Write(i);
+				Console.ForegroundColor = ConsoleColor.Gray;
+				if(i != 25)
+					Console.Write("|");
+			}
+			Console.WriteLine();
 			
 			int day = 0;
-			while(day == 0)
+			while(days[day] == null)
 			{
 				Console.Write("Choose a day to solve: ");
-				try
+				string input = Console.ReadLine();
+				
+				if(!Int32.TryParse(input, out day) || day < 1 || day > 25)
 				{
-					int input = Int32.Parse(Console.ReadLine());
-					if(days[input] == null)
-						Console.WriteLine("There is no solution for that day's puzzle yet! Sorry!");
-					else
-						day = input;
-				}
-				catch(Exception)
-				{
+					day = 0;
 					Console.WriteLine("Only numbers between 1 and 25 please!");
 				}
+				else if(days[day] == null)
+					Console.WriteLine("There is no solution for that day's puzzle yet! Sorry!");
 			}
 			
-			DateTime startTime = DateTime.Now;
+			Console.ForegroundColor = ConsoleColor.Green;
+			var watch = Stopwatch.StartNew();
 			try
 			{
 				days[day].Solve();
@@ -43,7 +56,9 @@ namespace AdventOfCode
 			{
 				Console.WriteLine(e);
 			}
-			Console.WriteLine("Solution took {0} milliseconds.", (DateTime.Now - startTime).Milliseconds);
+			watch.Stop();
+			Console.ForegroundColor = ConsoleColor.Gray;
+			Console.WriteLine("Solution took {0} milliseconds.", watch.ElapsedMilliseconds);
 			Console.WriteLine("Press any key to continue...");
 			Console.ReadKey();
 		}
