340ea60556bb8b1960a5773c20af4fd4b6b38288
gardenapple <mailbox@appl.garden>
2024-12-15 03:55:19 +0200

    Misunderstood the assignment, there was no need to do operator precedence

--- 2024/day7.c
+++ 2024/day7.c
@@ -7,18 +7,49 @@
 
 #include "aoc.h"
 
+typedef enum { ADD, MUL } op_t;
 
-static bool try_reach_result(int32_t const * nums, size_t i, size_t nums_n, int64_t required_result, int64_t current_value) {
-	if (i == nums_n)
-		return current_value == required_result;
-	if (current_value > required_result)
+static bool try_reach_result(int32_t const * nums, size_t nums_n, size_t i, 
+		int64_t prev_term, op_t op, int64_t term,
+		int64_t required_result, bool try_concat) {
+	fprintf(stderr, "i: %zu, (%" PRId64 " %c) %" PRId64 "\n", i, prev_term, op == ADD ? '+' : '*', term);
+	int64_t current_result;
+	switch (op) {
+		case ADD:
+			current_result = prev_term + term;
+			break;
+		case MUL:
+			current_result = prev_term * term;
+			break;
+	}
+
+	if (i + 1 == nums_n)
+		return current_result == required_result;
+	if (current_result > required_result)
 		return false;
 
-	int32_t num = nums[i];
-	if (try_reach_result(nums, i + 1, nums_n, required_result, current_value + num))
+	int32_t next_num = nums[i + 1];
+	if (try_reach_result(nums, nums_n, i + 1, current_result, ADD, next_num, required_result, try_concat))
 		return true;
-	if (try_reach_result(nums, i + 1, nums_n, required_result, current_value * num))
+	if (try_reach_result(nums, nums_n, i + 1, current_result, MUL, next_num, required_result, try_concat))
 		return true;
+
+	if (try_concat) {
+		fprintf(stderr, "%" PRId64 " || %" PRId32, term, next_num);
+		int32_t term_digit_calc = next_num;
+		while (term_digit_calc > 0) {
+			term_digit_calc /= 10;
+			term *= 10;
+			if (term > required_result) {
+				fprintf(stderr, " = (too much)\n");
+				return false;
+			}
+		}
+		term += next_num;
+		fprintf(stderr, " = %" PRId64 "\n", term);
+		if (try_reach_result(nums, nums_n, i + 1, prev_term, op, term, required_result, true))
+			return true;
+	}
 	return false;
 }
 
@@ -49,8 +80,8 @@ static int64_t solve(char const * const * lines, size_t lines_n, size_t longest_
 			aoc_err(err, "No numbers?");
 			return -1;
 		}
-		fprintf(stderr, "%zu numbers, first is %" PRId32 "\n", nums_n, nums[0]);
-		bool found = try_reach_result(nums, 1, nums_n, result, nums[0]);
+		// fprintf(stderr, "%zu numbers, first is %" PRId32 "\n", nums_n, nums[0]);
+		bool found = try_reach_result(nums, nums_n, 0, 0, ADD, nums[0], result, part == 2);
 		if (found) {
 			found_results_sum += result;
 			fprintf(stderr, "Found for line %s\n", line);
@@ -60,5 +91,5 @@ static int64_t solve(char const * const * lines, size_t lines_n, size_t longest_
 }
 
 int main(int argc, char * argv[]) {
-	aoc_main_parse_lines(argc, argv, 1, solve);
+	aoc_main_parse_lines(argc, argv, 2, solve);
 }
