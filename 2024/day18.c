e367fb34ec1b1a9102cba079fbc2a4b7ad237210
gardenapple <mailbox@appl.garden>
2024-12-21 23:52:22 +0200

    2024 day 18 part 2

--- 2024/day18.c
+++ 2024/day18.c
@@ -9,10 +9,12 @@
 #define PART1_OBSTACLES 1024
 #define WIDTH 71
 #define HEIGHT 71
+#define X_START 0
+#define Y_START 0
 #define X_END (WIDTH - 1)
 #define Y_END (HEIGHT - 1)
 
-static void shortest_path(int32_t * distances, int32_t x, int32_t y, int32_t current_dist, int32_t * min_dist) {
+static void shortest_path_(int32_t * distances, int32_t x, int32_t y, int32_t current_dist, int32_t * min_dist) {
 	// fprintf(stderr, "i'm walking 'ere: 'x: %"PRId32", y: %"PRId32", dist: %"PRId32"\n", x, y, current_dist);
 	size_t x_y_i = aoc_index_2d(WIDTH, x, y);
 	distances[x_y_i] = current_dist;
@@ -36,38 +38,144 @@ static void shortest_path(int32_t * distances, int32_t x, int32_t y, int32_t cur
 		size_t x_y_next_i = aoc_index_2d(WIDTH, x_next, y_next);
 		if (distances[x_y_next_i] <= next_dist)
 			continue;
-		shortest_path(distances, x_next, y_next, next_dist, min_dist);
+		shortest_path_(distances, x_next, y_next, next_dist, min_dist);
 	}
 }
 
+static int32_t shortest_path(int32_t * distances) {
+	for (size_t x_y_i= 0; x_y_i < WIDTH * HEIGHT; ++x_y_i) {
+		if (distances[x_y_i] != -1)
+			distances[x_y_i] = INT32_MAX;
+	}
+	int32_t min_dist = INT32_MAX;
+	shortest_path_(distances, X_START, Y_START, 0, &min_dist);
+	return min_dist;
+}
+
+static bool walk_back(int32_t * distances, int32_t x, int32_t y, int32_t current_dist, bool debug) {
+	if (debug)
+		fprintf(stderr, "i'm walking 'ere: 'x: %"PRId32", y: %"PRId32", dist: %"PRId32"\n", x, y, current_dist);
+	if (x == X_START && y == Y_START) {
+		return true;
+	}
+
+	int32_t next_dist = current_dist - 1;
+	if (next_dist == -1)
+		return false;
+
+	for (aoc_dir4_t dir = 0; dir < 4; ++dir) {
+		int32_t x_next = x + aoc_dir4_x_diffs[dir];
+		int32_t y_next = y + aoc_dir4_y_diffs[dir];
+		if (x_next < 0 || x_next >= WIDTH || y_next < 0 || y_next >= HEIGHT)
+			continue;
+
+		size_t x_y_next_i = aoc_index_2d(WIDTH, x_next, y_next);
+		if (distances[x_y_next_i] != next_dist)
+			continue;
+		bool walked = walk_back(distances, x_next, y_next, next_dist, debug);
+		if (walked)
+			return true;
+		// break;
+	}
+	return false;
+}
+
 static int64_t solve(char * const * lines, size_t lines_n, size_t const * line_lengths, int32_t part, aoc_ex_t * e) {
-	size_t obstacles = (lines_n > PART1_OBSTACLES) ? PART1_OBSTACLES : lines_n;
+	int32_t result = 0;
 
 	int32_t * distances = assert_calloc(WIDTH * HEIGHT, int32_t);
 
+	size_t obstacles = (lines_n > PART1_OBSTACLES) ? PART1_OBSTACLES : lines_n;
 	for (size_t i = 0; i < obstacles; ++i) {
 		int32_t x, y;
 		if (sscanf(lines[i], "%" PRId32 ",%" PRId32, &x, &y) != 2) {
 			aoc_throw(e, AOC_EX_DATA_ERR, AOC_MSG_INVALID_FILE);
 			goto cleanup;
 		}
+		if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT) {
+			aoc_throw(e, AOC_EX_DATA_ERR, AOC_MSG_INVALID_FILE);
+			goto cleanup;
+		}
 		fprintf(stderr, "x: %" PRId32 ", y: %" PRId32 "\n", x, y);
 		distances[aoc_index_2d(WIDTH, x, y)] = -1;
 	}
-	for (size_t y = 0; y < HEIGHT; ++y) {
-		for (size_t x = 0; x < WIDTH; ++x) {
-			size_t x_y_i = aoc_index_2d(WIDTH, x, y);
-			if (distances[x_y_i] != -1)
-				distances[x_y_i] = INT32_MAX;
-		}
+
+	int32_t min_dist = shortest_path(distances);
+	bool can_walk_back = walk_back(distances, X_END, Y_END, min_dist, true);
+	if (!can_walk_back) {
+		aoc_throw_fail(e, "bruh");
+		return result;
 	}
-	int32_t min_dist = INT32_MAX;
-	shortest_path(distances, 0, 0, 0, &min_dist);
+				for (size_t y = 0; y < HEIGHT; ++y) {
+					for (size_t x = 0; x < WIDTH; ++x) {
+						if (distances[aoc_index_2d(WIDTH, x, y)] == -1)
+							fputc('#', stderr);
+						else
+							fputc('.', stderr);
+					}
+					fputc('\n', stderr);
+				}
+
+	if (part == 1) {
+		result = min_dist;
+	} else {
+		fprintf(stderr, "Shortest path has length %"PRId32"\n", min_dist);
+
+		int32_t x = X_END, y = Y_END;
+		for (size_t i = PART1_OBSTACLES; i < lines_n; ++i) {
+			if (sscanf(lines[i], "%" PRId32 ",%" PRId32, &x, &y) != 2) {
+				aoc_throw(e, AOC_EX_DATA_ERR, AOC_MSG_INVALID_FILE);
+				goto cleanup;
+			}
+			fprintf(stderr, "x: %" PRId32 ", y: %" PRId32 "\n", x, y);
+			if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT) {
+				aoc_throw(e, AOC_EX_DATA_ERR, AOC_MSG_INVALID_FILE);
+				goto cleanup;
+			}
+			if (x == X_END && y == Y_END)
+				break;
+			distances[aoc_index_2d(WIDTH, x, y)] = -1;
+			// if ((x == 16 && y == 35) || (x == 18 && y == 43) || (x == 39 && y == 40)) {
+			// 	for (size_t y = 0; y < HEIGHT; ++y) {
+			// 		for (size_t x = 0; x < WIDTH; ++x) {
+			// 			if (x == 39 && y == 40)
+			// 				fputc('J', stderr);
+			// 			else if (distances[aoc_index_2d(WIDTH, x, y)] == -1)
+			// 				fputc('#', stderr);
+			// 			else
+			// 				fputc('.', stderr);
+			// 		}
+			// 		fputc('\n', stderr);
+			// 	}
+			// 	// reset and find another path
+			// 	min_dist = shortest_path(distances);
+			// 	fprintf(stderr, "dist: %"PRId32"\n", min_dist);
+			// 	if (min_dist == INT32_MAX) {
+			// 		fputs("lolblocked!\n", stderr);
+			// 		break;
+			// 	}
+			// }
+			bool can_walk_back = walk_back(distances, X_END, Y_END, min_dist, false);
+			if (can_walk_back) {
+				fputs("old path still works\n", stderr);
+				continue;
+			}
+			fputs("trying new path...", stderr);
+			// reset and find another path
+			min_dist = shortest_path(distances);
+			if (min_dist == INT32_MAX) {
+				fputs("blocked!\n", stderr);
+				break;
+			}
+			fprintf(stderr, "found! new dist is: %"PRId32"\n", min_dist);
+		}
+		result = 100 * x + y;
+	}
 cleanup:
 	free(distances);
-	return min_dist;
+	return result;
 }
 
 int main(int argc, char * argv[]) {
-	aoc_main_lines(argc, argv, 1, solve);
+	aoc_main_lines(argc, argv, 2, solve);
 }
--- 2024/test.sh
+++ 2024/test.sh
@@ -9,13 +9,16 @@ if [ -z "$2" ]; then
 	days="$1"
 	for day in $(seq 1 "$days"); do
 		input="day${day}.txt"
-		gcc aoc.c "day${day}.c" -march=native -O2 -Wall -std=c11 -o prog && time ./prog "$input"
+		gcc aoc.c "day${day}.c" -march=native -O2 -Wall -std=c11 -o prog 
+		time ./prog "$input"
 		echo "Day ${day} done."
-		read -r press_enter_to_continue
+		read -r _
 	done
 else
 	day="$1"
 	input="$2"
-	gcc aoc.c "day${day}.c" -march=native -O2 -Wall -std=c11 -o prog && time ./prog "$input"
-	echo "Day ${day} done."
+	gcc aoc.c "day${day}.c" -march=native -O2 -Wall -std=c11 -o prog 
+	echo "Compiled."
+	read -r _
+	time ./prog "$input"
 fi
