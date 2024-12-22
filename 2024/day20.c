0ece335f7d34575a2ce3dae11330e812512990bd
gardenapple <mailbox@appl.garden>
2024-12-22 04:26:46 +0200

    2024 day 20 part 2

--- 2024/day20.c
+++ 2024/day20.c
@@ -48,21 +48,21 @@ static int64_t solve(aoc_c32_2d_t maze, int32_t part, aoc_ex_t * e) {
 			aoc_throw(e, AOC_EX_DATA_ERR, "no valid dirs");
 			return -1;
 		}
-		for (int32_t sq = 0; sq < 8; ++sq) {
-			int32_t x_sq = x + aoc_sq8_x_diffs[sq];
-			int32_t y_sq = y + aoc_sq8_y_diffs[sq];
-			size_t x_y_sq_i = aoc_index_2d(maze.width, x_sq, y_sq);
-			if (path_matrix[x_y_sq_i] == -1)
-				continue;
-			if (path_matrix[x_y_sq_i] == current_dist - 1 || path_matrix[x_y_sq_i] == current_dist - 2)
-				continue;
-			if (abs(x_sq - x_next) > 1 || abs(y_sq - y_next) > 1) {
-				fprintf(stderr, "x: %"PRId32" y: %"PRId32"\n", x, y);
-				fprintf(stderr, "x: %"PRId32" y: %"PRId32"\n", x_sq, y_sq);
-				aoc_throw(e, AOC_EX_DATA_ERR, "diagonal shortcuts!");
-				return -1;
-			}
-		}
+		// for (int32_t sq = 0; sq < 8; ++sq) {
+		// 	int32_t x_sq = x + aoc_sq8_x_diffs[sq];
+		// 	int32_t y_sq = y + aoc_sq8_y_diffs[sq];
+		// 	size_t x_y_sq_i = aoc_index_2d(maze.width, x_sq, y_sq);
+		// 	if (path_matrix[x_y_sq_i] == -1)
+		// 		continue;
+		// 	if (path_matrix[x_y_sq_i] == current_dist - 1 || path_matrix[x_y_sq_i] == current_dist - 2)
+		// 		continue;
+		// 	if (abs(x_sq - x_next) > 1 || abs(y_sq - y_next) > 1) {
+		// 		fprintf(stderr, "x: %"PRId32" y: %"PRId32"\n", x, y);
+		// 		fprintf(stderr, "x: %"PRId32" y: %"PRId32"\n", x_sq, y_sq);
+		// 		aoc_throw(e, AOC_EX_DATA_ERR, "diagonal shortcuts!");
+		// 		return -1;
+		// 	}
+		// }
 		valid_dir = -1;
 		++current_dist;
 		x = x_next;
@@ -77,6 +77,7 @@ static int64_t solve(aoc_c32_2d_t maze, int32_t part, aoc_ex_t * e) {
 	// 	fputc('\n', stderr);
 	// }
 
+	int64_t search_radius = (part == 1) ? 2 : 20;
 	int64_t cheats_found = 0;
 
 	x = x_start;
@@ -94,19 +95,28 @@ static int64_t solve(aoc_c32_2d_t maze, int32_t part, aoc_ex_t * e) {
 				y_next = y_dir;
 			}
 
-			int32_t x_cheat = x_dir + aoc_dir4_x_diffs[dir];
-			int32_t y_cheat = y_dir + aoc_dir4_y_diffs[dir];
-			if (!aoc_check_bounds(maze, x_cheat, y_cheat))
-				continue;
-			size_t x_y_cheat_i = aoc_index_2d(maze.width, x_cheat, y_cheat);
-			if (path_matrix[x_y_cheat_i] - current_dist >= 2 + CHEAT_MIN) {
-				// fprintf(stderr, "saves %"PRId32"\n", (path_matrix[x_y_cheat_i] - current_dist - 2));
-				++cheats_found;
-			}
 		}
-		if (x_next == -1) {
-			aoc_throw_fail(e, "wut");
-			return -1;
+
+		int32_t y_cheat_min = aoc_max_int32_t(0, y - search_radius);
+		int32_t y_cheat_max = aoc_min_int32_t(maze.height - 1, y + search_radius);
+
+		for (int32_t y_cheat = y_cheat_min; y_cheat <= y_cheat_max; ++y_cheat) {
+			int32_t search_remaining = search_radius - abs(y_cheat - y);
+
+			int32_t x_cheat_min = aoc_max_int32_t(0, x - search_remaining);
+			int32_t x_cheat_max = aoc_min_int32_t(maze.width - 1, x + search_remaining);
+
+			for (int32_t x_cheat = x_cheat_min; x_cheat <= x_cheat_max; ++x_cheat) {
+				int32_t cheat_length = abs(x_cheat - x) + abs(y_cheat - y);
+				size_t x_y_cheat_i = aoc_index_2d(maze.width, x_cheat, y_cheat);
+				if (path_matrix[x_y_cheat_i] - current_dist >= cheat_length + CHEAT_MIN) {
+					// fprintf(stderr, "x: %"PRId32" y: %"PRId32" cx: %"PRId32" cy: %"PRId32" len: %"PRId32"\n",
+					// 		x, y, x_cheat, y_cheat, cheat_length);
+					// fprintf(stderr, "saves %"PRId32"\n", (path_matrix[x_y_cheat_i] - current_dist - cheat_length));
+					++cheats_found;
+				}
+			}
+
 		}
 		x = x_next;
 		y = y_next;
@@ -117,5 +127,5 @@ static int64_t solve(aoc_c32_2d_t maze, int32_t part, aoc_ex_t * e) {
 }
 
 int main(int argc, char * argv[]) {
-	aoc_main_c32_2d(argc, argv, 1, solve);
+	aoc_main_c32_2d(argc, argv, 2, solve);
 }
