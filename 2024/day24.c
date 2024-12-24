a912765a0ea5c02f15128ea3df4bd0c1cf641e12
gardenapple <mailbox@appl.garden>
2024-12-25 00:57:48 +0200

    2024 day 24 reformat to use array of gates

--- 2024/day24.c
+++ 2024/day24.c
@@ -9,7 +9,6 @@
 #define ALPHANUMS 36
 #define WIRES_COUNT (ALPHANUMS * ALPHANUMS * ALPHANUMS)
 
-AOC_ASSERT_NULL_IS_ZERO
 AOC_ASSERT_SANE_ENCODING
 
 
@@ -21,19 +20,18 @@ typedef struct gate {
 	gate_op_t op;
 	int out;
 
-	struct gate * next;
 	// pointers to next unsolved gates
-	struct gate * next_left_in;
-	struct gate * next_right_in;
+	ptrdiff_t next_left_in;
+	ptrdiff_t next_right_in;
 } gate_t;
 
-typedef enum : char { VAL_EMPTY = 0, VAL_FALSE, VAL_TRUE } wire_val_t;
+typedef enum : char { VAL_NOT_INIT = 0, VAL_EMPTY, VAL_FALSE, VAL_TRUE } wire_val_t;
 
 typedef struct {
 	wire_val_t value;
 	// pointers to next unsolved gates
-	gate_t * next_left_in;
-	gate_t * next_right_in;
+	ptrdiff_t next_left_in;
+	ptrdiff_t next_right_in;
 } wire_t;
 
 
@@ -61,7 +59,8 @@ static void fprint(gate_t gate) {
 			out);
 }
 
-static bool try_eval(wire_t * wires, gate_t gate) {
+static bool try_eval(wire_t * wires, gate_t * gates, size_t i) {
+	gate_t gate = gates[i];
 	bool left_in;
 	switch (wires[gate.left_in].value) {
 		case VAL_FALSE:
@@ -97,6 +96,7 @@ static bool try_eval(wire_t * wires, gate_t gate) {
 			break;
 	}
 
+	bool out_wire_init = wires[gate.out].value != VAL_NOT_INIT;
 	wires[gate.out].value = out ? VAL_TRUE : VAL_FALSE;
 	
 	fprint(gate);
@@ -105,39 +105,48 @@ static bool try_eval(wire_t * wires, gate_t gate) {
 	else
 		fputs(" (= FALSE)\n", stderr);
 
-	gate_t ** in_gate = &(wires[gate.out].next_left_in);
-	while (*in_gate) {
-		if (try_eval(wires, **in_gate)) {
-			fprint(**in_gate);
+	if (!out_wire_init)
+		return true;
+
+	ptrdiff_t * in_gate_i = &(wires[gate.out].next_left_in);
+	while (*in_gate_i != -1) {
+		fprint(gates[*in_gate_i]);
+		fprintf(stderr, " - gate (i = %zd)\n", *in_gate_i);
+		if (try_eval(wires, gates, *in_gate_i)) {
+			fprint(gates[*in_gate_i]);
 			fprintf(stderr, " - removing\n");
 			// remove gate from next_left chain
-			*in_gate = (*in_gate)->next_left_in;
+			*in_gate_i = gates[*in_gate_i].next_left_in;
 		} else {
 			// skip to next gate
-			in_gate = &((*in_gate)->next_left_in);
+			in_gate_i = &(gates[*in_gate_i].next_left_in);
 		}
 	}
 
-	in_gate = &(wires[gate.out].next_right_in);
-	while (*in_gate) {
-		if (try_eval(wires, **in_gate)) {
-			fprint(**in_gate);
+	in_gate_i = &(wires[gate.out].next_right_in);
+	while (*in_gate_i != -1) {
+		if (try_eval(wires, gates, *in_gate_i)) {
+			fprint(gates[*in_gate_i]);
 			fprintf(stderr, " - removing\n");
 			// remove gate from next_right chain
-			*in_gate = (*in_gate)->next_right_in;
+			*in_gate_i = gates[*in_gate_i].next_right_in;
 		} else {
 			// skip to next gate
-			in_gate = &((*in_gate)->next_right_in);
+			in_gate_i = &(gates[*in_gate_i].next_right_in);
 		}
 	}
 
 	return true;
 }
 
+static void parse_gates(char const * const * lines, size_t const * line_lengths, size_t n, 
+		gate_t * gates, wire_t * wires, aoc_ex_t * e);
+static void sort_gates(gate_t * gates, size_t n, wire_t * wires);
+static void eval_gates(gate_t * gates, size_t n, wire_t * wires);
+
 
 static int64_t solve(char const * const * lines, size_t lines_n, size_t const * line_lengths, int32_t part, aoc_ex_t * e) {
 	wire_t * wires = assert_calloc(WIRES_COUNT, wire_t);
-	gate_t * gates = NULL;
 	fprintf(stderr, "%zu\n", sizeof(wire_t));
 
 	size_t line_i = 0;
@@ -146,81 +155,34 @@ static int64_t solve(char const * const * lines, size_t lines_n, size_t const * 
 		int wire_num = strtoul(lines[line_i], &end, ALPHANUMS);
 		if (wire_num >= WIRES_COUNT) {
 			aoc_throw_invalid_file(e);
-			goto cleanup;
+			goto cleanup_wires;
 		}
-		// aoc_bit_array_set(has_value, wire_num, true);
 
 		if (lines[line_i][5] == '1')
 			wires[wire_num].value = VAL_TRUE;
 		else
 			wires[wire_num].value = VAL_FALSE;
+		wires[wire_num].next_left_in = -1;
+		wires[wire_num].next_right_in = -1;
 
 		++line_i;
 	}
 
-
-	// for (size_t i = 0; i < WIRES_MAX; ++i) {
-	// 	if (aoc_bit_array_get(has_value, i)) {
-	// 		char wire_name[4];
-	// 		wire_num_to_name(i, wire_name);
-	// 		if (aoc_bit_array_get(value, i)) {
-	// 			fprintf(stderr, "%s -> 1\n", wire_name);
-	// 		} else {
-	// 			fprintf(stderr, "%s -> 0\n", wire_name);
-	// 		}
-	// 	}
-	// }
 	
 	++line_i;
 	if (line_i >= lines_n) {
 		aoc_throw_invalid_file(e);
 		goto cleanup;
 	}
+	size_t gates_n = lines_n - line_i;
+	gate_t * gates = assert_malloc(gates_n, gate_t);
 
-	do {
-		if (line_lengths[line_i] < 17) {
-			aoc_throw_invalid_file(e);
-			goto cleanup;
-		}
-		// strtoul may skip one whitespace, if the op was 2 chars instead of 3
-		gate_t gate = {
-			.left_in = strtoul(lines[line_i], NULL, ALPHANUMS),
-			.right_in = strtoul(lines[line_i] + 7, NULL, ALPHANUMS),
-			.out = strtoul(lines[line_i] + 14, NULL, ALPHANUMS)
-		};
-		char const * op_str = lines[line_i] + 4;
-		if (strncmp(op_str, "AND", 3) == 0) {
-			gate.op = AND;
-		} else if (strncmp(op_str, "OR", 2) == 0) {
-			gate.op = OR;
-		} else if (strncmp(op_str, "XOR", 3) == 0) {
-			gate.op = XOR;
-		} else {
-			aoc_throw_invalid_file(e);
-			goto cleanup;
-		}
+	parse_gates(lines + line_i, line_lengths + line_i, gates_n, gates, wires, e);
+	if (*e) {
+		goto cleanup;
+	}
 
-		if (try_eval(wires, gate)) {
-			// no need to store it for later
-			goto continue_next_line;
-		} else {
-			fprint(gate);
-			fprintf(stderr, " - storing for later...\n");
-			gate_t * next_gate = gates;
-			gates = assert_malloc(1, gate_t);
-			*gates = gate;
-			gates->next = next_gate;
-
-			gates->next_left_in = wires[gate.left_in].next_left_in;
-			wires[gate.left_in].next_left_in = gates;
-
-			gates->next_right_in = wires[gate.right_in].next_right_in;
-			wires[gate.right_in].next_right_in = gates;
-		}
-
-continue_next_line:
-		++line_i;
-	} while (line_i < lines_n);
+	// sort_gates(gates, gates_n, wires);
 
 	int64_t result = 0;
 	int z00_wire = (ALPHANUMS - 1) * ALPHANUMS * ALPHANUMS;
@@ -248,15 +210,72 @@ continue_next_line:
 end_bits:;
 
 cleanup:;
-	gate_t * gate = gates;
-	while (gate) {
-		gate_t * next_gate = gate->next;
-		free(gate);
-		gate = next_gate;
-	}
+	free(gates);
+cleanup_wires:
 	free(wires);
 	return result;
 }
+
+static void parse_gates(char const * const * lines, size_t const * line_lengths, size_t n, 
+		gate_t * gates, wire_t * wires, aoc_ex_t * e) {
+	size_t i = 0;
+	do {
+		if (line_lengths[i] < 17) {
+			aoc_throw_invalid_file(e);
+			return;
+		}
+		// strtoul may skip one whitespace, if the op was 2 chars instead of 3
+		gate_t gate = {
+			.left_in = strtoul(lines[i], NULL, ALPHANUMS),
+			.right_in = strtoul(lines[i] + 7, NULL, ALPHANUMS),
+			.out = strtoul(lines[i] + 14, NULL, ALPHANUMS)
+		};
+		char const * op_str = lines[i] + 4;
+		if (strncmp(op_str, "AND", 3) == 0) {
+			gate.op = AND;
+		} else if (strncmp(op_str, "OR", 2) == 0) {
+			gate.op = OR;
+		} else if (strncmp(op_str, "XOR", 3) == 0) {
+			gate.op = XOR;
+		} else {
+			aoc_throw_invalid_file(e);
+			return;
+		}
+		if (gate.left_in >= WIRES_COUNT || gate.right_in >= WIRES_COUNT || gate.out >= WIRES_COUNT) {
+			aoc_throw_invalid_file(e);
+			return;
+		}
+
+		gates[i] = gate;
+
+		if (!try_eval(wires, gates, i)) {
+			fprint(gate);
+			fprintf(stderr, " - storing for later...\n");
+			if (wires[gate.left_in].value == VAL_NOT_INIT) {
+				wires[gate.left_in].value = VAL_EMPTY;
+				wires[gate.left_in].next_left_in = i;
+				wires[gate.left_in].next_right_in = -1;
+				gates[i].next_left_in = -1;
+			} else {
+				gates[i].next_left_in = wires[gate.left_in].next_left_in;
+				wires[gate.left_in].next_left_in = i;
+			}
+
+			if (wires[gate.right_in].value == VAL_NOT_INIT) {
+				wires[gate.right_in].value = VAL_EMPTY;
+				wires[gate.right_in].next_left_in = -1;
+				wires[gate.right_in].next_right_in = i;
+				gates[i].next_right_in = -1;
+			} else {
+				gates[i].next_right_in = wires[gate.right_in].next_right_in;
+				wires[gate.right_in].next_right_in = i;
+			}
+		}
+
+		++i;
+	} while (i < n);
+}
+
 
 int main(int argc, char * argv[]) {
 	aoc_main_const_lines(argc, argv, 1, solve);
